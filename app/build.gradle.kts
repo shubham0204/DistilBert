32c34c37c29015f1092a49609b4b9b2de186bb80
Georgios <farmaker47@gmail.com>
2024-01-20 17:35:42 +0200

    Initial commit

---
+++ app/build.gradle.kts
@@ -0,0 +1,104 @@
+import org.jetbrains.kotlin.de.undercouch.gradle.tasks.download.Download
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.example.distilbert"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.distilbert"
+        minSdk = 28
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+tasks {
+    val downloadModel by creating(Download::class) {
+        onlyIf { !file("$projectDir/src/main/assets/model.tflite").exists() }
+        // https://s3.amazonaws.com/models.huggingface.co/bert/distilbert-base-uncased-distilled-squad-384-fp16.tflite    // FP16 quantization version
+        // "https://s3.amazonaws.com/models.huggingface.co/bert/distilbert-base-uncased-distilled-squad-384-8bits.tflite" // hybrid quantization version
+        src("https://s3.amazonaws.com/models.huggingface.co/bert/distilbert-base-uncased-distilled-squad-384.tflite")
+        dest("$projectDir/src/main/assets/model.tflite")
+    }
+    whenTaskAdded {
+        if (name in listOf("assembleDebug", "assembleRelease")) {
+            dependsOn(downloadModel)
+        }
+    }
+}
+
+dependencies {
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    implementation("androidx.compose.material3:material3")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
+    debugImplementation("androidx.compose.ui:ui-tooling")
+    debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+    // DistilBert
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.annotation:annotation:1.7.1")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("com.google.android.material:material:1.11.0")
+
+    implementation("com.google.code.gson:gson:2.10.1")
+    implementation("com.google.guava:guava:28.1-android")
+    // implementation("org.tensorflow:tensorflow-lite:2.0.0")
+    // implementation("org.tensorflow:tensorflow-lite-select-tf-ops:2.9.0")
+    implementation(files("libs/tensorflow-lite-with-select-tf-ops-0.0.0-nightly.aar"))
+
+    testImplementation("com.google.truth:truth:1.0")
+    testImplementation("org.robolectric:robolectric:4.3.1")
+    androidTestImplementation("androidx.test:runner:1.5.2")
+}
